621340f89514f68992617b9eac8a24c0c8b147d1
mateusz <m.sierakowski@op.pl>
2024-05-12 17:59:35 +0200

    dodanie zadania 3 i 4

--- src/Main.kt
+++ src/Main.kt
@@ -22,6 +22,27 @@ fun <A> isSorted(aa: List<A>, order: (A, A) -> Boolean): Boolean {
     }
     return true
 }
+// zadanie 3
+fun square(numbers: List<Number>): List<Number>{
+    val result = mutableListOf<Number>()
+    for(i in numbers.indices) {
+        if(i%2 != 0 && numbers[i] is Double && numbers[i].toDouble() > 0 ){
+            result.add(numbers[i].toDouble() * numbers[i].toDouble())
+        } else if (i%2 != 0 && numbers[i] is Int && numbers[i].toInt() > 0 ) {
+
+        result.add(numbers[i].toInt() * numbers[i].toInt())}
+    }
+    return result
+}
+// zadanie 4
+fun <A> tailToHead(lst:List<A>):List<A>{
+    if(lst.isEmpty()){
+        throw IllegalArgumentException("Lista jest pusta")
+    }
+    val lastElement=lst.last()
+    val modifiedList=lst.dropLast(1)
+    return listOf(lastElement)+modifiedList
+}
 
 
 fun main() {
@@ -34,4 +55,16 @@ fun main() {
     println(isSorted(listOf(1,2,3,4), {i:Int, j:Int ->i <j }))//true
     println(isSorted(listOf(1,1,1,1), {i:Int, j:Int->i==j }))//true
     print(isSorted(listOf("ahyyhh", "bkjn", "cnn", "duu"), {i:String, j:String->i.first()<j.first()}))// true
+    println("\n")
+
+    // zadanie 3 sprawdzanie
+    val result = square(listOf(1,2,3.5,5,-6,1,1))
+    println(result)
+
+    // zadanie 4 sprawdzanie
+    val result_2=tailToHead(listOf(1,2,3))
+    println(result_2)
+
+
+
 }
